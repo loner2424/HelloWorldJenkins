f59e46a0650bba139cf62d9e6c747600f1a99ef5
loner2424 <92987477+loner2424@users.noreply.github.com>
2023-11-11 10:00:52 +0530

    Update Main.java
    Happy diwali everyone

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! in Jenkins, by Sahil, Arya , Divya and Bhushan"); 
+        System.out.println("Hello, World! in Jenkins, by Sahil, Arya , Divya and Bhushan, Happy Diwali"); 
     }
 }
